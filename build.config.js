c53279dd962d6007480344ff3780f15a3f17c33d
MorevM <max.seainside@gmail.com>
2022-04-09 08:43:19 +0300

    chore: Migrate to `unbuild`

---
+++ build.config.js
@@ -0,0 +1,18 @@
+import { execSync } from 'child_process';
+
+export default {
+	rollup: {
+		emitCJS: true,
+	},
+	entries: [
+		'./src/index',
+		'./src/bundlers',
+		'./src/vue',
+		'./src/nuxt',
+	],
+	hooks: {
+		'build:done': (ctx) => {
+			execSync('cp ./src/_nuxt-plugin.js ./dist/_nuxt-plugin.js');
+		},
+	},
+};
